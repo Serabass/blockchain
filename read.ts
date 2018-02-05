c9da674610041ae5f9736a0f983242f25e8654f4
Serabass <is_everything@mail.ru>
2018-02-06 01:30:31 +0600

    update

--- read.ts
+++ read.ts
@@ -2,5 +2,5 @@ import {BlockChain} from "./blockchain";
 
 (async () => {
     let chain = await BlockChain.fromFile("block.txt");
-    console.log("Chain correctivity: ", chain.check());
+    console.log("Chain is correct: ", chain.check());
 })();
