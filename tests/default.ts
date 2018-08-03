58a4a0b38d805e090e0cb7397860206972423628
Serabass <>
2018-08-03 10:59:16 +0600

    upd

--- tests/default.ts
+++ tests/default.ts
@@ -4,12 +4,12 @@ import {BlockChain} from "../blockchain";
 import {StringTransaction} from "../transactions/string-transaction";
 
 describe("Hello function", () => {
-    let blockChain: BlockChain;
+    let blockChain: BlockChain<string>;
     beforeEach(() => {
         blockChain = new BlockChain();
     });
 
-    it("Test 1", () => {
+    it("Should create one transaction", () => {
         let stringTransaction = new StringTransaction(blockChain);
         stringTransaction.data = "hello!";
         stringTransaction.datetime = new Date();
@@ -18,10 +18,10 @@ describe("Hello function", () => {
         expect(blockChain.length).to.eq(1);
     });
 
-    it("Test 2", () => {
+    it("Should fail on change transaction data", () => {
         for (let i = 0; i < 10; i++) {
             let stringTransaction = new StringTransaction(blockChain);
-            stringTransaction.data = "hello!";
+            stringTransaction.data = Math.random().toString(16);
             stringTransaction.datetime = new Date();
             stringTransaction.hash = stringTransaction.buildHash();
             blockChain.addTransaction(stringTransaction);
@@ -29,7 +29,27 @@ describe("Hello function", () => {
 
         expect(blockChain.check()).to.eq(true);
 
-        blockChain.transactions[0].data = "Kill me";
+        let randomIndex = Math.round(Math.random() * blockChain.transactions.length - 1);
+
+        blockChain.transactions[randomIndex].data = "Kill me";
+
+        expect(blockChain.check()).to.eq(false);
+    });
+
+    it("Should fail on change transaction datetime", () => {
+        for (let i = 0; i < 10; i++) {
+            let stringTransaction = new StringTransaction(blockChain);
+            stringTransaction.data = Math.random().toString(16);
+            stringTransaction.datetime = new Date();
+            stringTransaction.hash = stringTransaction.buildHash();
+            blockChain.addTransaction(stringTransaction);
+        }
+
+        expect(blockChain.check()).to.eq(true);
+
+        let randomIndex = Math.round(Math.random() * blockChain.transactions.length - 1);
+
+        blockChain.transactions[randomIndex].datetime.setFullYear(0);
 
         expect(blockChain.check()).to.eq(false);
     });
